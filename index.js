f1b47491910395901a0a9879bd2b380f0558ff68
Brian <deltoro.brian@gmail.com>
2017-10-26 23:24:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+//mykittens = ["Milo", "Otis", "Garfield"];
+
+function kittens(){
+  return mykittens;
+}
+
+function destructivelyAppendKitten(name){
+  mykittens.push(name);
+  return mykittens;
+}
+
+function destructivelyPrependKitten(name){
+  mykittens.unshift(name);
+  return mykittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  mykittens.pop();
+  return mykittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  mykittens.shift();
+  return mykittens;
+}
+
+function appendKitten(name){
+  var newkittens = [...mykittens, name]
+  return newkittens;
+}
